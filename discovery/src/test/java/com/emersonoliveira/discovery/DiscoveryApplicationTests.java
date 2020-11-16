62be6d07b668880b60dce3474409d6adbfc03f51
emerson.oliveira <emerson.oliveira2@engdb.com.br>
2020-11-16 14:18:20 -0300

    Commit inicial microsevice discovery

---
+++ discovery/src/test/java/com/emersonoliveira/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emersonoliveira.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
