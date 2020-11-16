348cb8419eddd68f14c7927b67e889166cdcd7d3
emerson.oliveira <emerson.oliveira2@engdb.com.br>
2020-11-16 15:00:30 -0300

    Registros dos microservices no servidor eureka.

--- auth/src/main/java/com/emersonoliveira/auth/AuthApplication.java
+++ auth/src/main/java/com/emersonoliveira/auth/AuthApplication.java
@@ -9,10 +9,12 @@ import java.util.Objects;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthApplication {
 
     public static final String USER_ADMIN = "admin";
--- auth/src/main/java/com/emersonoliveira/auth/jwt/JwtTokenFilter.java
+++ auth/src/main/java/com/emersonoliveira/auth/jwt/JwtTokenFilter.java
@@ -9,7 +9,6 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
